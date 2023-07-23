381bdd7e52155b2c297a3d26be4eb8541127830c
Omar ESSOUFI <omar.essoufi@gmail.com>
2023-07-23 14:55:05 +0100

    finish hero section

--- src/sections/heroSection/HeroSection.js
+++ src/sections/heroSection/HeroSection.js
@@ -2,25 +2,35 @@ import React from "react";
 import styles from "./HeroSection.module.scss";
 import backLargeImg from "../../assets/images/home-slider-1.jpg";
 import backMobileImg from "../../assets/images/home-slider-1-mb.jpg";
-
+import affirmImg from "../../assets/images/affirm.png";
 const HeroSection = () => {
   return (
-    <div className={styles.hero}>
-      <div className={styles.largeImg}>
-        <img src={backLargeImg} alt="techshop slider" />
-      </div>
-      <div className={styles.mobileImg}>
-        <img src={backMobileImg} alt="techshop slider" />
+    <>
+      <div className={styles.hero}>
+        <div className={styles.largeImg}>
+          <img src={backLargeImg} alt="techshop slider" />
+        </div>
+        <div className={styles.mobileImg}>
+          <img src={backMobileImg} alt="techshop slider" />
+        </div>
+
+        <div className={styles.titles}>
+          <h1>
+            <span>Huge Saving</span> on UHD Televisions
+          </h1>
+          <p>Sale up to 70% off on selected items*</p>
+          <button className="--btn">Shop Now</button>
+        </div>
       </div>
 
-      <div className={styles.titles}>
-        <h1>
-          <span>Huge Saving</span> on UHD Televisions
-        </h1>
-        <p>Sale up to 70% off on selected items*</p>
-        <button className="--btn">Shop Now</button>
+      <div className={styles.affirm}>
+        <div className={styles.logo}>
+          <img src={affirmImg} alt="Affirm" />
+        </div>
+        <p>BUY NOW, PAY LATER STARTING AT 0% APR</p>
+        <button>LEARN MORE</button>
       </div>
-    </div>
+    </>
   );
 };
 
