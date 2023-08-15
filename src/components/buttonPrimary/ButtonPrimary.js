3a4f402ec426cb3ad8df7af6121b8b396981f8d3
Omar ESSOUFI <omar.essoufi@gmail.com>
2023-08-15 10:10:18 +0100

    build register page

--- src/components/buttonPrimary/ButtonPrimary.js
+++ src/components/buttonPrimary/ButtonPrimary.js
@@ -1,16 +1,18 @@
 import React from "react";
 import styles from "./ButtonPrimary.module.scss";
 
-const ButtonPrimary = ({ text, type, onClick, disabled }) => {
+const ButtonPrimary = ({ text, type, onClick, disabled, className }) => {
   return (
-    <button
-      className={`--btn ${styles.button}`}
-      type={type}
-      onClick={onClick}
-      disabled={disabled}
-    >
-      {text}
-    </button>
+    <div className={className}>
+      <button
+        className={`--btn ${styles.button}`}
+        type={type}
+        onClick={onClick}
+        disabled={disabled}
+      >
+        {text}
+      </button>
+    </div>
   );
 };
 
--- src/pages/createAccount/CreateAccount.js
+++ src/pages/createAccount/CreateAccount.js
@@ -1,23 +1,38 @@
 import React from "react";
 import styles from "./CreateAccount.module.scss";
 import BreadCrumb from "../../components/breadCrumb/BreadCrumb";
+import Input from "../../components/input/Input";
+import ButtonPrimary from "../../components/buttonPrimary/ButtonPrimary";
+import registerImg from "../../assets/images/login/login-cuate-edited.png";
 
 const CreateAccount = () => {
   return (
     <>
       <BreadCrumb page1={"Register"} />
       <div className={styles.createAccount}>
+        <div className={styles.img}>
+          <img src={registerImg} alt="register" />
+        </div>
         <div className={styles.card}>
           <h2>Create Account</h2>
           <p>Please register below to create an account</p>
           <form>
-            <input type="text" placeholder="First Name" required />
-            <input type="text" placeholder="Last Name." />
-            <input type="email" placeholder="Email Address" required />
-            <input type="password" placeholder="Password" required />
-            <input type="checkbox" id="emailMarketing" />
-            <label htmlFor="emailMarketing">Subscribe To Email Marketing</label>
-            <button>Create An Account</button>
+            <Input type="text" placeholder="First Name" required={true} />
+            <Input type="text" placeholder="Last Name." />
+            <Input type="email" placeholder="Email Address" required={true} />
+            <Input type="password" placeholder="Password" required={true} />
+            <div className={styles.marketing}>
+              <input
+                className="form-check-input"
+                type="checkbox"
+                id="emailMarketing"
+              />
+              <label htmlFor="emailMarketing">
+                Subscribe To Email Marketing
+              </label>
+            </div>
+
+            <ButtonPrimary text={"Create An Account"} />
           </form>
         </div>
       </div>
