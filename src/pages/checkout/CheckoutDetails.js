7a8239225799d3079f482447beff81dcce8495b3
Omar ESSOUFI <omar.essoufi@gmail.com>
2023-08-25 23:39:19 +0100

    create checkoutDetails

--- src/pages/checkout/CheckoutDetails.js
+++ src/pages/checkout/CheckoutDetails.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { selectTotalAmount, selectTotalQuantity } from "../../redux/cartSlice";
+import styles from "./CheckoutDetails.module.scss";
 
 const CheckoutDetails = () => {
   const totalQuantity = useSelector(selectTotalQuantity);
   const totalAmount = useSelector(selectTotalAmount);
   return (
-    <div>
-      <h1>{totalQuantity}</h1>
-      <h1>{totalAmount}</h1>
+    <div className={styles.checkoutDetails}>
+      <h2>Checkout Details</h2>
+      <div className={styles.container}>
+        <div className={styles.card}>
+          <h4>Shipping Address</h4>
+        </div>
+      </div>
     </div>
   );
 };
