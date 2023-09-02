08c21f8c17b59b2bb2a0185c5634a2a6ce2024e1
Omar ESSOUFI <omar.essoufi@gmail.com>
2023-09-02 16:05:28 +0100

    handle add default address and update

--- src/pages/myAddress/MyAddress.js
+++ src/pages/myAddress/MyAddress.js
@@ -6,7 +6,7 @@ import Input from "../../components/input/Input";
 import { CountryDropdown } from "react-country-region-selector";
 import ButtonPrimary from "../../components/buttonPrimary/ButtonPrimary";
 import { useDispatch, useSelector } from "react-redux";
-import { selectAddress, selectUserID } from "../../redux/authSlice";
+import { selectUserID } from "../../redux/authSlice";
 import { SAVE_SHIPPING_ADDRESS } from "../../redux/checkoutSlice";
 import { toast } from "react-toastify";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
@@ -25,6 +25,8 @@ const shippingInit = {
 
 const MyAddress = () => {
   const [shippingAddress, setShippingAddress] = useState(shippingInit);
+  const [showForm, setShowForm] = useState(false);
+  const [showAddress, setShowAddress] = useState(true);
   const uid = useSelector(selectUserID);
   const dispatch = useDispatch();
 
@@ -35,6 +37,10 @@ const MyAddress = () => {
 
       if (docSnap.exists()) {
         setShippingAddress(docSnap.data().address);
+        if (!docSnap.data().address.line1) {
+          setShowAddress(false);
+          setShowForm(true);
+        }
       } else {
         // docSnap.data() will be undefined in this case
         console.log("No such document!");
@@ -52,14 +58,24 @@ const MyAddress = () => {
     await updateDoc(userRef, {
       address: shippingAddress,
     });
+    setShowForm(false);
+    setShowAddress(true);
     toast.success("Your Address is saved successfully.");
+
+    //scroll to top
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   };
 
   const handleInput = (e) => {
     setShippingAddress({ ...shippingAddress, [e.target.name]: e.target.value });
   };
 
-  const updateHandler = () => {};
+  const updateHandler = () => {
+    setShowForm(true);
+  };
 
   //scroll to top
   useEffect(() => {
@@ -73,152 +89,157 @@ const MyAddress = () => {
     <div className={`--container ${styles.myAddress}`}>
       <h2>My Shipping Address</h2>
       <div className={styles.content}>
-        <div className={styles.defaultAddress}>
-          <p>Your default shipping address is:</p>
-          <div className={styles.card}>
-            <table>
-              <thead></thead>
-              <tbody>
-                <tr>
-                  <th>
-                    <b>Name</b>
-                  </th>
-                  <td>{shippingAddress.name}</td>
-                </tr>
-                <tr>
-                  <th>
-                    <b>Address</b>
-                  </th>
-                  <td>
-                    {shippingAddress.line1 + ", " + shippingAddress.line2}
-
-                    <br />
-                    {shippingAddress.postal_code + ", " + shippingAddress.city}
-
-                    <br />
-                    {shippingAddress.state}
-                  </td>
-                </tr>
-                <tr>
-                  <th>
-                    <b>Country</b>
-                  </th>
-                  <td>{shippingAddress.country}</td>
-                </tr>
-                <tr>
-                  <th>
-                    <b>Phone</b>
-                  </th>
-                  <td>{shippingAddress.phone}</td>
-                </tr>
-              </tbody>
-            </table>
-            <div className={styles.btnUpdate}>
-              <ButtonPrimary
-                text={"Update Address"}
-                onClick={updateHandler}
-                style={{ maxWidth: "180px", padding: "7px" }}
-              />
-            </div>
-          </div>
-        </div>
-        <div className={styles.newAddress}>
-          <p>Save your default shipping address:</p>
-          <form onSubmit={submitHandler}>
+        {showAddress && (
+          <div className={styles.defaultAddress}>
+            <p>Your default shipping address is:</p>
             <div className={styles.card}>
-              <label>Recipient Name</label>
-              <Input
-                type="text"
-                placeholder="Recipient Name"
-                required
-                name="name"
-                value={shippingAddress.name}
-                onChange={(e) => handleInput(e)}
-                className={styles.input}
-              />
-              <label>Address Line 1</label>
-              <Input
-                type="text"
-                placeholder="Address Line 1"
-                required
-                name="line1"
-                value={shippingAddress.line1}
-                onChange={(e) => handleInput(e)}
-                className={styles.input}
-              />
-              <label>Address Line 2</label>
-              <Input
-                type="text"
-                placeholder="Address Line 2"
-                name="line2"
-                value={shippingAddress.line2}
-                onChange={(e) => handleInput(e)}
-                className={styles.input}
-              />
-              <label>City</label>
-              <Input
-                type="text"
-                placeholder="City"
-                required
-                name="city"
-                value={shippingAddress.city}
-                onChange={(e) => handleInput(e)}
-                className={styles.input}
-              />
-              <label>State</label>
-              <Input
-                type="text"
-                placeholder="State"
-                required
-                name="state"
-                value={shippingAddress.state}
-                onChange={(e) => handleInput(e)}
-                className={styles.input}
-              />
-              <label>Postal Code</label>
-              <Input
-                type="text"
-                placeholder="Postal Code"
-                required
-                name="postal_code"
-                value={shippingAddress.postal_code}
-                className={styles.input}
-                onChange={(e) => handleInput(e)}
-              />
-              {/* Country Input */}
-              <label>Country</label>
-              <div
-                className="--rounded --light-border"
-                style={{ padding: "5px 10px", marginTop: "5px" }}
-              >
-                <CountryDropdown
-                  className={styles.select}
-                  valueType="short"
-                  value={shippingAddress.country}
-                  onChange={(val) =>
-                    handleInput({
-                      target: {
-                        name: "country",
-                        value: val,
-                      },
-                    })
-                  }
+              <table>
+                <tbody>
+                  <tr>
+                    <th scope="row">
+                      <b>Name</b>
+                    </th>
+                    <td>{shippingAddress.name}</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">
+                      <b>Address</b>
+                    </th>
+                    <td>
+                      {shippingAddress.line1 + ", " + shippingAddress.line2}
+
+                      <br />
+                      {shippingAddress.postal_code +
+                        ", " +
+                        shippingAddress.city}
+
+                      <br />
+                      {shippingAddress.state}
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">
+                      <b>Country</b>
+                    </th>
+                    <td>{shippingAddress.country}</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">
+                      <b>Phone</b>
+                    </th>
+                    <td>{shippingAddress.phone}</td>
+                  </tr>
+                </tbody>
+              </table>
+              <div className={styles.btnUpdate}>
+                <ButtonPrimary
+                  text={"Update Address"}
+                  onClick={updateHandler}
+                  style={{ maxWidth: "180px", padding: "7px" }}
                 />
               </div>
-              <label>Phone</label>
-              <Input
-                type="text"
-                placeholder="Phone"
-                required
-                name="phone"
-                value={shippingAddress.phone}
-                onChange={(e) => handleInput(e)}
-                className={styles.input}
-              />
-              <ButtonPrimary text={"Save Address"} />
             </div>
-          </form>
-        </div>
+          </div>
+        )}
+        {showForm && (
+          <div className={styles.newAddress}>
+            <p>Save your default shipping address:</p>
+            <form onSubmit={submitHandler}>
+              <div className={styles.card}>
+                <label>Recipient Name</label>
+                <Input
+                  type="text"
+                  placeholder="Recipient Name"
+                  required
+                  name="name"
+                  value={shippingAddress.name}
+                  onChange={(e) => handleInput(e)}
+                  className={styles.input}
+                />
+                <label>Address Line 1</label>
+                <Input
+                  type="text"
+                  placeholder="Address Line 1"
+                  required
+                  name="line1"
+                  value={shippingAddress.line1}
+                  onChange={(e) => handleInput(e)}
+                  className={styles.input}
+                />
+                <label>Address Line 2</label>
+                <Input
+                  type="text"
+                  placeholder="Address Line 2"
+                  name="line2"
+                  value={shippingAddress.line2}
+                  onChange={(e) => handleInput(e)}
+                  className={styles.input}
+                />
+                <label>City</label>
+                <Input
+                  type="text"
+                  placeholder="City"
+                  required
+                  name="city"
+                  value={shippingAddress.city}
+                  onChange={(e) => handleInput(e)}
+                  className={styles.input}
+                />
+                <label>State</label>
+                <Input
+                  type="text"
+                  placeholder="State"
+                  required
+                  name="state"
+                  value={shippingAddress.state}
+                  onChange={(e) => handleInput(e)}
+                  className={styles.input}
+                />
+                <label>Postal Code</label>
+                <Input
+                  type="text"
+                  placeholder="Postal Code"
+                  required
+                  name="postal_code"
+                  value={shippingAddress.postal_code}
+                  className={styles.input}
+                  onChange={(e) => handleInput(e)}
+                />
+                {/* Country Input */}
+                <label>Country</label>
+                <div
+                  className="--rounded --light-border"
+                  style={{ padding: "5px 10px", marginTop: "5px" }}
+                >
+                  <CountryDropdown
+                    className={styles.select}
+                    valueType="short"
+                    value={shippingAddress.country}
+                    onChange={(val) =>
+                      handleInput({
+                        target: {
+                          name: "country",
+                          value: val,
+                        },
+                      })
+                    }
+                  />
+                </div>
+                <label>Phone</label>
+                <Input
+                  type="text"
+                  placeholder="Phone"
+                  required
+                  name="phone"
+                  value={shippingAddress.phone}
+                  onChange={(e) => handleInput(e)}
+                  className={styles.input}
+                />
+                <ButtonPrimary text={"Save Address"} />
+              </div>
+            </form>
+          </div>
+        )}
       </div>
     </div>
   );
