719b02069a6b8e1e0889173fc3a1c97f0d056098
Omar ESSOUFI <omar.essoufi@gmail.com>
2023-09-14 12:18:09 +0100

    add links to Main Side Menu

--- src/components/header/mainMenu/MainMenu.js
+++ src/components/header/mainMenu/MainMenu.js
@@ -8,40 +8,67 @@ import {
 } from "react-icons/md";
 import { TbInfoSquareRounded } from "react-icons/tb";
 import { HiOutlineHome } from "react-icons/hi";
+import { useNavigate } from "react-router-dom";
 
 const MainMenu = ({ show, onHide, position, title }) => {
+  const navigate = useNavigate();
+
+  const toHome = () => {
+    navigate("/");
+    onHide();
+  };
+
+  const toHotDeal = () => {
+    navigate("/hot-deal");
+    onHide();
+  };
+
+  const toCollections = () => {
+    navigate("/collections");
+    onHide();
+  };
+
+  const toAboutUs = () => {
+    navigate("/about-us");
+    onHide();
+  };
+
+  const toContact = () => {
+    navigate("/contact");
+    onHide();
+  };
   return (
     <SideMenu show={show} onHide={onHide} position={position} title={title}>
       <ul className={styles.navMenu}>
-        <li>
+        <li onClick={toHome}>
           <span>
             <HiOutlineHome size={24} />
           </span>
           <span>Home</span>
         </li>
 
-        <li>
+        <li onClick={toHotDeal}>
           <span>
             <RiFlashlightLine size={24} />
           </span>
           <span>Hot Deal</span>
         </li>
 
-        <li>
+        <li onClick={toCollections}>
           <span>
             <MdOutlineCollectionsBookmark size={24} />
           </span>
           <span>Collection</span>
         </li>
 
-        <li>
+        <li onClick={toAboutUs}>
           <span>
             <TbInfoSquareRounded size={24} />
           </span>
           <span>About Us</span>
         </li>
 
-        <li>
+        <li onClick={toContact}>
           <span>
             <MdOutlineMarkEmailRead size={24} />
           </span>
