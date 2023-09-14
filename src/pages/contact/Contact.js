edf2e8e074d92ebafb024f671224a3f43703806f
Omar ESSOUFI <omar.essoufi@gmail.com>
2023-09-14 09:05:29 +0100

    add reset form and Loader after sending email

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -3,16 +3,18 @@ import ButtonPrimary from "../../components/buttonPrimary/ButtonPrimary";
 import { FaPhone, FaLocationDot, FaTwitter } from "react-icons/fa6";
 import { IoMail } from "react-icons/io5";
 import Input from "../../components/input/Input";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import { toast } from "react-toastify";
+import Loader from "../../components/loader/Loader";
 
 const Contact = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
-    console.log(form.current);
+    setIsLoading(true);
 
     emailjs
       .sendForm(
@@ -24,65 +26,73 @@ const Contact = () => {
       .then(
         (result) => {
           toast.success("Your email sent successfully.");
+          form.current.reset();
         },
         (error) => {
           toast.error(error.message);
         }
-      );
+      )
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
   return (
-    <div className={`--container ${styles.contact}`}>
-      <h2>Contact Us</h2>
-      <div className={styles.wrapper}>
-        <div className={styles.card}>
-          <form ref={form} onSubmit={sendEmail}>
-            <label>Name: </label>
-            <Input type="text" name="user_name" required={true} />
+    <>
+      <div className={`--container ${styles.contact}`}>
+        <h2>Contact Us</h2>
+        <div className={styles.wrapper}>
+          <div className={styles.card}>
+            <form ref={form} onSubmit={sendEmail}>
+              <label>Name: </label>
+              <Input type="text" name="user_name" required={true} />
 
-            <label>Email: </label>
-            <Input type="email" name="user_email" required={true} />
+              <label>Email: </label>
+              <Input type="email" name="user_email" required={true} />
 
-            <label>Subject: </label>
-            <Input type="text" name="subject" required={true} />
+              <label>Subject: </label>
+              <Input type="text" name="subject" required={true} />
 
-            <label>Message: </label>
-            <textarea rows="10" name="message" required={true} />
+              <label>Message: </label>
+              <textarea rows="10" name="message" required={true} />
 
-            <div style={{ display: "flex", justifyContent: "right" }}>
-              <ButtonPrimary
-                text={"Send Message"}
-                style={{ width: "180px" }}
-                type={"submit"}
-              />
+              <div style={{ display: "flex", justifyContent: "right" }}>
+                <ButtonPrimary
+                  text={"Send Message"}
+                  style={{ width: "180px" }}
+                  type={"submit"}
+                />
+              </div>
+            </form>
+          </div>
+
+          <div className={styles.info}>
+            <h5>Our Contact Informations</h5>
+            <p>Fill the form or contact us via other channels listed bellow</p>
+            <div className={styles.icon}>
+              <FaPhone size={16} color="#fff" />
+              <span>(00 216)695 985 69</span>
             </div>
-          </form>
-        </div>
 
-        <div className={styles.info}>
-          <h5>Our Contact Informations</h5>
-          <p>Fill the form or contact us via other channels listed bellow</p>
-          <div className={styles.icon}>
-            <FaPhone size={16} color="#fff" />
-            <span>(00 216)695 985 69</span>
-          </div>
+            <div className={styles.icon}>
+              <IoMail size={16} color="#fff" />
+              <span>omar.essoufi@gmail.com</span>
+            </div>
 
-          <div className={styles.icon}>
-            <IoMail size={16} color="#fff" />
-            <span>omar.essoufi@gmail.com</span>
-          </div>
+            <div className={styles.icon}>
+              <FaLocationDot size={16} color="#fff" />
+              <span>Ariana, Tunisia</span>
+            </div>
 
-          <div className={styles.icon}>
-            <FaLocationDot size={16} color="#fff" />
-            <span>Ariana, Tunisia</span>
-          </div>
-
-          <div className={styles.icon}>
-            <FaTwitter size={16} color="#fff" />
-            <span>@Omar__ESSOUFI</span>
+            <div className={styles.icon}>
+              <FaTwitter size={16} color="#fff" />
+              <span>@Omar__ESSOUFI</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+
+      {isLoading && <Loader />}
+    </>
   );
 };
 
